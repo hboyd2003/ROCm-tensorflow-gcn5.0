47e229228864b5cd4932f52c4aafd9a62f664d4e
hboyd2003 <8950185+hboyd2003@users.noreply.github.com>
2025-04-12 00:15:03 +0000

    Configure Dockerfiles and build to target only GCN5.0 GPUs

--- tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
+++ tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
@@ -137,7 +137,7 @@ elif [[ "$DISTRO" == "el8" ]]; then
 fi
 
 
-GPU_DEVICE_TARGETS=${GPU_DEVICE_TARGETS:-"gfx908 gfx90a gfx942 gfx1030 gfx1100 gfx1200 gfx1201"}
+GPU_DEVICE_TARGETS=${GPU_DEVICE_TARGETS:-"gfx900 gfx902 gfx906"}
 
 echo $ROCM_VERSION
 echo $ROCM_REPO
