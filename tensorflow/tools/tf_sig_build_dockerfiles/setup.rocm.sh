5769b59c138b188eb964ab7c9b16b86fe8cfa263
hboyd2003 <8950185+hboyd2003@users.noreply.github.com>
2025-04-12 00:15:03 +0000

    Configure Dockerfiles and build to target only GCN5.0 GPUs

--- tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
+++ tensorflow/tools/tf_sig_build_dockerfiles/setup.rocm.sh
@@ -145,7 +145,7 @@ then
   echo "build:rocm_base --copt=-fclang-abi-compat=17" >> /etc/bazel.bazelrc
 fi
 
-GPU_DEVICE_TARGETS=${GPU_DEVICE_TARGETS:-"gfx908 gfx90a gfx942 gfx1030 gfx1100"}
+GPU_DEVICE_TARGETS=${GPU_DEVICE_TARGETS:-"gfx900 gfx902 gfx906"}
 
 echo $ROCM_VERSION
 echo $ROCM_REPO
